c36448e106af1cbdf74aac28e2e46795a817f48a
Zhu Zhanyan <program.nom@gmail.com>
2023-06-02 08:43:28 +0800

    fix(nvim): restore nvim-dap use{} section mistakenly deleted in 3c6290b

--- .config/nvim/lua/plugins/language/dap.lua
+++ .config/nvim/lua/plugins/language/dap.lua
@@ -7,7 +7,68 @@
 local dap = {}
 
 function dap.use_plugins(use)
-    -- js debugging
+    use {
+        "mfussenegger/nvim-dap",
+        tag = "0.6.0",
+        requires = { "williamboman/mason.nvim" },
+        run = function()
+            require("mason.api.command").MasonInstall {
+                "cpptools", -- c,c++,rust
+            }
+        end,
+        config = function()
+            local d = require("dap")
+            local dap_widgets = require("dap.ui.widgets")
+            local install_path = require("plugins.mason").install_path
+
+            -- c, c++, rust
+            d.adapters.cppdbg = {
+                id = "cppdbg",
+                type = "executable",
+                command = install_path("cpptools") .. "/extension/debugAdapters/bin/OpenDebugAD7",
+            }
+            local cppdbg = {
+                {
+                    name = "Launch file",
+                    type = "cppdbg",
+                    request = "launch",
+                    program = function()
+                        -- prompt user for executable to debug
+                        return vim.fn.input('Executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                }
+            }
+            d.configurations.c = cppdbg
+            d.configurations.cpp = cppdbg
+            d.configurations.rust = cppdbg
+
+            for key, dap_fn in pairs({
+                    ["<leader>dc"] = d.continue,
+                    ["<leader>dn"] = d.step_over,
+                    ["<leader>ds"] = d.step_into,
+                    ["<leader>do"] = d.step_out,
+                    ["<leader>*"] = d.toggle_breakpoint,
+                    ["<leader>dd"] = function()
+                        -- populate quickfix window with breakpoints and show them
+                        d.list_breakpoints()
+                        vim.cmd.copen()
+                    end,
+                    ["<leader>D"] = d.clear_breakpoints,
+                    ["<leader>d:"] = d.repl.open,
+                    ["<leader>d<cr>"] = d.run_last,
+                    ["<Leader>dk"] = dap_widgets.hover,
+                    ["<Leader>df"] = function()
+                        dap_widgets.centered_float(dap_widgets.frames)
+                    end,
+                    ["<Leader>dS"] = function()
+                        dap_widgets.centered_float(dap_widgets.scopes)
+                    end,
+                }) do
+                vim.keymap.set({ 'n' }, key, dap_fn, { noremap = true })
+            end
+        end
+    }    -- js debugging
     use {
         "mxsdev/nvim-dap-vscode-js",
         tag = "v1.1.0",
