15cc18e486ecb4b68da3bc117d04bd1b3db7e032
Zhu Zhanyan <program.nom@gmail.com>
2025-04-07 22:32:27 +0800

    refactor(nvim): remove unused obsidian plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -53,43 +53,8 @@ lazy.setup({
 			require("indent_blankline").setup({})
 		end,
 	},
-
-	-- Obsidian notes
-	{
-		"epwalsh/obsidian.nvim",
-		-- lazy load plugin only when opening a note from vault
-		lazy = true,
-		event = {
-			"BufReadPre " .. obsidian_vault_dir .. "**/*.md",
-			"BufNewFile " .. obsidian_vault_dir .. "**/*.md",
-		},
-		tag = "v1.14.2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function(_)
-			local obsidian = require("obsidian")
-			obsidian.setup({
-				dir = obsidian_vault_dir,
-				disable_frontmatter = true,
-				daily_notes = {
-					folder = "journal",
-				},
-				-- autocomplete wikilinks
-				completion = {
-					nvim_cmp = true,
-				},
-				-- disable automatic mappings
-				mappings = {},
-			})
-			-- key bindings
-			local map = vim.keymap.set
-			map({ "n" }, "<leader>o[", ":ObsidianBacklinks<CR>", {})
-			map({ "n" }, "<leader>oo", ":ObsidianOpen<CR>", {})
-			map({ "n" }, "<leader>oj", ":ObsidianToday<CR>", {})
-			-- override 'gf' for navigating wikilinks navigation
-			map({ "n" }, "gf", obsidian.util.gf_passthrough, { noremap = false, expr = true })
-		end,
-	},
-
 	-- Common utilities shared by plugins
 	{ "nvim-lua/plenary.nvim", tag = "v0.1.3" },
+
+
 })
