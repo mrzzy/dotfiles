6d9ed482dbfb1304507a0654bce35608ab9b6d50
Zhu Zhanyan <program.nom@gmail.com>
2026-06-04 14:30:51 +0800

    refactor(nvim): remove deprecated dap config

--- .config/nvim/lua/plugins/language/dap.lua
+++ .config/nvim/lua/plugins/language/dap.lua
@@ -6,18 +6,6 @@
 
 local M = {}
 
-function M.reload_launch_json()
-	-- load launch.json debugging if one exists
-	local launch_json = vim.fn.getcwd() .. "/.vscode/launch.json"
-	if vim.fn.filereadable(launch_json) then
-		require("dap.ext.vscode").load_launchjs(launch_json, {
-			-- adapters -> filetype mapping
-			cppdbg = { "c", "cpp", "rust" },
-			["pwa-node"] = { "javascript", "typescript" },
-		})
-	end
-end
-
 return {
 	{
 		"mfussenegger/nvim-dap",
@@ -34,12 +22,10 @@ return {
 				type = "executable",
 				command = install_path("cpptools") .. "/extension/debugAdapters/bin/OpenDebugAD7",
 			}
-			M.reload_launch_json()
 
 			-- debugging key bindings
 			for key, dap_fn in pairs({
 				["<leader>dc"] = function()
-					M.reload_launch_json()
 					d.continue()
 				end,
 				["<leader>dn"] = d.step_over,
